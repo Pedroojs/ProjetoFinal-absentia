fc7796c70a2c4003c1ce0876a232d29157c625bb
Pedroojs <pholiveiramachado@gmail.com>
2026-02-25 21:42:59 -0300

    Start entity User

--- demo/src/main/java/com/sgf/app/model/User.java
+++ demo/src/main/java/com/sgf/app/model/User.java
@@ -1,5 +1,46 @@
 package com.sgf.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+
+
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
